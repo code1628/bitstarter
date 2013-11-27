7a9bebf0887dcbcc1d5363f43d332c59f9b9b6a9
code1628 <marian.stagarescu@gmail.com>
2013-11-27 20:09:43 +0000

    updated web.js

--- web.js
+++ web.js
@@ -2,9 +2,13 @@ var express = require('express');
 var app = express();
 app.use(express.logger());
 
+var infile = "/home/ubuntu/bitstarter/index.html";
+
+
 app.get('/', function(request, response) {
-  response.send('Hello World!');
+  response.send(fs.fileReadSync(infile)));
 });
+
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
