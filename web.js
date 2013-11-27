dea8f1375b3622deec3c5d3d9046a1bd64864e91
code1628 <marian.stagarescu@gmail.com>
2013-11-27 20:55:58 +0000

    using bitstarter.html

--- web.js
+++ web.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 app.use(express.logger());
 
-var infile = "/home/ubuntu/bitstarter/index.html";
+var infile = "/home/ubuntu/bitstarter/bitstarter.html";
 
 
 app.get('/', function(request, response) {
